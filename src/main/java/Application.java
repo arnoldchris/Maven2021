315db832dceda0e23b6e40575be76d7ca473ba8e
chris <arnold937@yahoo.com>
2021-06-26 20:08:32 +0100

    Project Created with the maven project.

---
+++ src/main/java/Application.java
@@ -0,0 +1,18 @@
+public class Application {
+
+    int i;
+    final int j=10;
+    public void test(){
+
+    }
+    public void test(String str){
+
+    }
+    public void test(String str1, int i){
+
+    }
+
+    public void doSomething(){
+        System.out.println("Doing Something");
+    }
+}
---
+++ src/test/java/Test_Application.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+public class Test_Application {
+    @Test
+    public void test1(){
+        System.out.println("Running Test 1");
+    }
+    @Test
+    public void test2(){
+        System.out.println("Running Test 2");
+    }
+    @Test
+    public void test3(){
+        System.out.println("Running Test 3");
+    }
+    @Test
+    public void test4(){
+        System.out.println("Running Test 4");
+    }
+
+}
